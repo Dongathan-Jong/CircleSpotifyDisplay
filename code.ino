22a3e86ebd02639d6dbed370d680242c6d2e41c4
Dongathan-Jong <duclingsyt@gmail.com>
2025-08-01 16:38:03 -0400

    Update code.ino

--- code.ino
+++ code.ino
@@ -1,31 +1,42 @@
+#include <SpotifyEsp32.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_GC9A01A.h>
 #include <SPI.h>
 
-#define TFT_CS   5
-#define TFT_DC   2
-#define TFT_RST  4
+#define TFT_CS 5
+#define TFT_DC 2
+#define TFT_RST 4
+#define TFT_MOSI 8
+#define TFT_SCLK 10
 
-Adafruit_GC9A01A tft(TFT_CS, TFT_DC, TFT_RST);
+const char* SSID = "NETGEAR80";
+const char* PASSWORD = "tinybanana043";
 
-int ballX = 120;
-int ballY = 120;
-int ballRadius = 10;
-int speedX = 2;
-int speedY = 3;
-uint16_t ballColor = GC9A01A_RED;
-uint16_t bgColor = GC9A01A_BLACK;
+const char* CLIENT_ID = "";
+const char* CLIENT_SECRET = "";
+const char* REFRESH_TOKEN = "";
+
+Spotify sp(CLIENT_ID, CLIENT_SECRET);
+Adafruit_GC9A01A tft(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
+
 
 void setup() {
   Serial.begin(115200);
   delay(500);
-  
+
+  sp.begin();
+  while(!sp.is_auth())
+  {
+    sp.handle_client();
+  }
+  Serial.println("Auth");
   tft.begin();
   tft.setRotation(0);
-  tft.fillScreen(bgColor);
+  tft.fillScreen(GC9A01A_BLACK);
 }
 
 void loop() {
+
   tft.fillCircle(ballX, ballY, ballRadius, bgColor);
 
   ballX += speedX;
@@ -41,4 +52,5 @@ void loop() {
   tft.fillCircle(ballX, ballY, ballRadius, ballColor);
 
   delay(10); 
+  tft.drawCircle(120, 120, 50, GC9A01A_RED);
 }
